d4d79c9365e0efffd96a36e3b0e15762bb1048e9
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-05-28 23:44:03 +0530

    scoffold project and add trie implementation

---
+++ trie.js
@@ -0,0 +1,60 @@
+class TrieNode {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+    this.wordEnds = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert = function (word = "") {
+    let currentNode = this.root;
+    for (const letter of word) {
+      if (!currentNode.children.has(letter)) {
+        currentNode.children.set(letter, new TrieNode(letter));
+      }
+      currentNode = currentNode.children.get(letter);
+    }
+    currentNode.wordEnds = true;
+    return currentNode;
+  };
+
+  check = function (word = "") {
+    let currentNode = this.root;
+    for (const letter of word) {
+      if (!currentNode.children.has(letter)) return false;
+      currentNode = currentNode.children.get(letter);
+    }
+    return currentNode.wordEnds;
+  };
+
+  view = function (root = this.root, word = "", wordList = []) {
+    if (root.wordEnds) wordList.push(word);
+    root.children.forEach((child) => {
+      this.view(child, word + child.value, wordList);
+    });
+    return wordList;
+  };
+
+  getWordsWithPrefix = function (prefix = "") {
+    let currentNode = this.root;
+    for (const letter of prefix) {
+      if (!currentNode.children.has(letter)) return [];
+      currentNode = currentNode.children.get(letter);
+    }
+    return this.view(currentNode, prefix);
+  };
+}
+
+const trie = new Trie();
+trie.insert("hello");
+trie.insert("world");
+trie.insert("height");
+trie.insert("waddle");
+
+console.log(trie.view());
+console.log(trie.getWordsWithPrefix("he"));
